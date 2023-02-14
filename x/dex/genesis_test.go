f84b66409f8a05329b461e9d45f9e24af94768b9
TimNhanTa <tim@unigrid.org>
2023-02-14 16:46:10 +0100

    ignite scaffold module dex --ibc

---
+++ x/dex/genesis_test.go
@@ -0,0 +1,31 @@
+package dex_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	keepertest "vesting/testutil/keeper"
+	"vesting/testutil/nullify"
+	"vesting/x/dex"
+	"vesting/x/dex/types"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+		PortId: types.PortID,
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.DexKeeper(t)
+	dex.InitGenesis(ctx, *k, genesisState)
+	got := dex.ExportGenesis(ctx, *k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	require.Equal(t, genesisState.PortId, got.PortId)
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}
